1bfffb44b0f53be77fb166c7ae0db8cbe40b68ba
Rovshan Maharramov <94802025+rouvsen@users.noreply.github.com>
2023-07-02 17:51:03 +0000

    simple algorithm added

---
+++ examples.cpp
@@ -0,0 +1,14 @@
+//Dev C++ <-- Compiler
+
+#include<stdio.h>
+
+int main()
+{ double x;
+double a=3,b=2;
+x=a/b;
+printf("%2.1lf",(int)x);
+return 0;}
+
+//result --> " 0.0"
+
+//Note: \a is alarm, you can use is it in f.e if(x == 1) printf("\a\n Hello");
